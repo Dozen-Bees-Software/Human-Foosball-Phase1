5b732145c37ddf414443b3e0ac14bef6e25169a5
Kyle Hamlin <khamlin95@ufl.edu>
2017-10-22 19:04:25 -0400

    Got some functionality commiting to save for work later (add players)

--- modules/core/client/controllers/playerList.client.controller.js
+++ modules/core/client/controllers/playerList.client.controller.js
@@ -5,10 +5,17 @@ angular.module('core').controller('PlayerListController', ['$scope', 'Authentica
     // This provides Authentication context.
     $scope.authentication = Authentication;
 
-    $scope.players = {};
+    $scope.players = [];
 
     $scope.addPlayer = function() {
-        $scope.players.push($scope)
-    }
+      $scope.newPlayer.elo = 0;
+      $scope.newPlayer.wins = 0;
+      $scope.newPlayer.losses= 0;
+      $scope.newPlayer.ties = 0;
+      $scope.newPlayer.differential = 0;
+      $scope.newPlayer.gamesPlayed = 0;
+      $scope.players.push($scope.newPlayer);
+      $scope.newPlayer = {};
+    };
   }
 ]);
