4f3f32b8e55fd457d2c765f7dd2a11da36f4925b
Byron George <Byron@Byrons-MacBook-Pro.local>
2017-10-26 20:58:09 -0400

    Got brackets controller to read tournamentInfo and playerlist

--- modules/core/client/controllers/brackets.client.controller.js
+++ modules/core/client/controllers/brackets.client.controller.js
@@ -6,6 +6,15 @@ angular.module('core').controller('BracketsController', ['$scope', 'Authenticati
 
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    //initialize playerlist, tournamentInfo, and team arrays
+    $scope.playerList = Players.getPlayerList();
+    console.log($scope.playerList[0]);
+    $scope.tournamentInfo = tournConfig.getTournamentInfo();
+    console.log($scope.tournamentInfo[1]);
+    $scope.team1 = [];
+    $scope.team2 = [];
+
   }
 
 ]);
