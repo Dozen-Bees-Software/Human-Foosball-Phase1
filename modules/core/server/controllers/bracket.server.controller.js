77668c910f32c3e9ade7debc15be08a8181136e1
David Fox <davidlorenfox@gmail.com>
2017-10-28 16:03:19 -0400

    Fixed syntax errors

--- modules/core/server/controllers/bracket.server.controller.js
+++ modules/core/server/controllers/bracket.server.controller.js
@@ -10,29 +10,29 @@ angular.module('core').controller('BracketController', ['$scope', 'Authenticatio
     $scope.rounds = 0;
 
     //Where do we get teamsize from?
-    var teamSize = floor($scope.players.length);
+    var teamSize = Math.floor($scope.players.length);
 
     $scope.generateElo = function(){
       var maxDifferential = 0;
-      for(var player in $scope.players)
+      for(var i = 0; i<$scope.players.size; i++)
       {
-        if(abs(player.differential)>maxDifferential)
+        if(Math.abs($scope.players[i].differential)>maxDifferential)
         {
-          maxDifferential = player.differential;
+          maxDifferential = $scope.players[i].differential;
         }
       }
 
-      var n = floor(Math.log10(maxDifferential))+1
+      var n = Math.floor(Math.log10(maxDifferential))+1;
 
-      for(var player in $scope.players)
+      for(i = 0; i < $scope.players.size; i++)
       {
-        player.elo = player.wins + (5*pow(10,n)+player.differential)/(10*pow(10,n))
+        $scope.players[i].elo = $scope.players[i].wins + (5*Math.pow(10,n)+$scope.players[i].differential)/(10*Math.pow(10,n));
       }
-    }
+    };
 
     $scope.sortTeams = function(){
       function compareGamesPlayed(a,b){
-        let comparison = 0;
+        var comparison = 0;
         if(a.gamesPlayed > b.gamesPlayed)
         {
           comparison = 1;
@@ -45,17 +45,17 @@ angular.module('core').controller('BracketController', ['$scope', 'Authenticatio
       }
 
       $scope.players.sort(compareGamesPlayed);
-    }
+    };
 
     $scope.generateTeams = function(){
       var lineup = $scope.players.slice(0,teamSize*2-1);
-      $scope.team1;
-      $scope.team2;
+      $scope.team1 = [];
+      $scope.team2 = [];
       //Need to get number of players playing and sort them based on compareElo
 
 
       function compareElo(a,b){
-        let comparison = 0;
+        var comparison = 0;
         if(a.elo > b.elo)
         {
           comparison = 1;
@@ -89,6 +89,6 @@ angular.module('core').controller('BracketController', ['$scope', 'Authenticatio
 
 
 
-    }
-}
+    };
+  }
 ]);
