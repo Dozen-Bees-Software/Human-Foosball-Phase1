9d95272539bb9f876385819bc4b5e9ae1e47ee17
the1337chef <the1337chef@ufl.edu>
2017-10-31 19:45:25 -0400

    merged theo_master and dev_byron

--- modules/core/client/services/tournamentConfig.client.service.js
+++ modules/core/client/services/tournamentConfig.client.service.js
@@ -7,11 +7,15 @@ angular.module('core').service('tournConfig', [
 
     this.addTournamentInfo = function(tournInformation) {
       tournamentInfo = tournInformation;
-      console.log(tournamentInfo[0]);
+      //console.log(tournamentInfo[0]);
     };
 
     this.getTournamentInfo = function() {
       return tournamentInfo;
+    };
+
+    this.updateRounds = function(){
+      tournamentInfo[4] += 1;
     };
 
 
